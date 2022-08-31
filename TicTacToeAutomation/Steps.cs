aa2b8192c007335a9bd31ef5611538b109756b45
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-31 15:54:18 +0200

    Going to the game

--- TicTacToeAutomation/Steps.cs
+++ TicTacToeAutomation/Steps.cs
@@ -1,7 +1,12 @@
-﻿namespace TicTacToeAutomation
+﻿using OpenQA.Selenium.Chrome;
+
+namespace TicTacToeAutomation
 {
     public class Steps : Elements
     {
-
+        public void GoToURL(ChromeDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://playtictactoe.org/");
+        }
     }
 }

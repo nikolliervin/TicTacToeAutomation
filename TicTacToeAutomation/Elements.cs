7b160dffce30a72f983dd2930064d63e1837fbcd
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-31 16:03:53 +0200

    Square Element

--- TicTacToeAutomation/Elements.cs
+++ TicTacToeAutomation/Elements.cs
@@ -1,6 +1,9 @@
-﻿namespace TicTacToeAutomation
+﻿using OpenQA.Selenium;
+
+namespace TicTacToeAutomation
 {
     public class Elements
     {
+        public static By SquareOfIndex(int index) => By.XPath($"/html/body/div[3]/div[1]/div[{index}]");
     }
 }
--- TicTacToeAutomation/TicTacToeAutomation.cs
+++ TicTacToeAutomation/TicTacToeAutomation.cs
@@ -4,9 +4,14 @@ namespace TicTacToeAutomation
 {
     public class TicTacToeAutomation
     {
+
         static void Main(string[] args)
         {
             var driver = new ChromeDriver();
+            var steps = new Steps();
+
+            steps.GoToURL(driver);
+
         }
     }
 }
